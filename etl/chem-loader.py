955487298245e5cbc60df4f9fc763e83ebfd5200
tdcrone <tdcrone@centos.home.crone.us>
2020-03-29 17:02:24 -0400

    Various loader variants - chemical-loader is primary

--- etl/chem-loader.py
+++ etl/chem-loader.py
@@ -1,4 +1,6 @@
 """
+MEMORY/SWAP VERSION
+
 This library contains tools for loading raw data from the ZINC15 dataset.
 
 It is expected that the ZINC15 3D data set is available at ../files.docking.org/3D/*
@@ -32,11 +34,74 @@ Features are one or several of:
   world
 """
 
+import concurrent.futures
 import logging
 import os
 import re
-import subprocess
 import sys
+import pyspark
+
+
+def feature_map(feature_list=None, feature_map=None):
+    """
+    Map features to bits or vice versa.  If feature_list is passed
+    then the bitmap is returned, if feature_map is passed then the
+    string form is returned.
+    :param feature_list: list of features in comma-delimited form
+    :type feature_list: str
+    :param feature_map: bitmap of features
+    :type feature_map: int
+    :return: bitmap or list of features
+    :rtype: int|str
+    """
+    KNOWN_FEATURES = {
+        'aggregator': 1,
+        'biogenic': 2,
+        'endogenous': 4,
+        'fda': 8,
+        'in-man': 16,
+        'investigational': 32,
+        'in-vitro': 64,
+        'in-vivo': 128,
+        'metabolite': 256,
+        'world': 512
+    }
+    _result = 0
+    if feature_list:
+        for feature in feature_list.split(','):
+            _result |= KNOWN_FEATURES[feature]
+    return _result
+
+
+def load_chem(filename):
+    """
+    Loads the chemical list from filename and returns the appropriate
+    dictionary.  Best to keep under the memory limit.
+    :param filename: file name to read
+    :type filename: str
+    :return: list of chemicals as defined above
+    :rtype: dict
+    """
+    _return = []
+    with open(filename, 'r') as chem_file:
+        chem_file.readline()  # throw out the header row
+        for line in chem_file:
+            chunks = line.split("\t")
+            _return += [
+                {
+                    'zinc': chunks[1],
+                    'smile': chunks[0],
+                    'tranche': chunks[12],
+                    'charge': chunks[5],
+                    'ph': chunks[6],
+                    'molwt': chunks[7],
+                    'logp': chunks[8],
+                    'reactive': chunks[10],
+                    'features': feature_map(chunks[11])
+
+                }
+            ]
+    return _return
 
 
 class ChemLoader:
@@ -59,6 +124,8 @@ class ChemLoader:
         :type tree: str
         """
         self.tranche = {}
+        _tranche_list = []
+        _tranche_files = []
         root_spec = re.compile("[A-Z]{2}")
         path_spec = re.compile("[A-Z]{4}")
         file_spec = re.compile("([A-Z]{6}).txt")
@@ -71,71 +138,13 @@ class ChemLoader:
                 for file_name in os.listdir(f"{tree}/{root_path}/{mid_path}"):
                     if not self.path_match(file_name, file_spec):
                         continue
-                    _tranche = file_name.split('/')[-1].split('.txt')[0]
-                    logging.info("Starting %s", _tranche)
-                    self.tranche[_tranche] = self.load_chem(f"{tree}/{root_path}/{mid_path}/{file_name}")
-                    logging.info("Completed %s", _tranche)
-
-    @staticmethod
-    def feature_map(feature_list=None, feature_map=None):
-        """
-        Map features to bits or vice versa.  If feature_list is passed
-        then the bitmap is returned, if feature_map is passed then the
-        string form is returned.
-        :param feature_list: list of features in comma-delimited form
-        :type feature_list: str
-        :param feature_map: bitmap of features
-        :type feature_map: int
-        :return: bitmap or list of features
-        :rtype: int|str
-        """
-        KNOWN_FEATURES = {
-            'aggregator': 1,
-            'biogenic': 2,
-            'endogenous': 4,
-            'fda': 8,
-            'in-man': 16,
-            'investigational': 32,
-            'in-vitro': 64,
-            'in-vivo': 128,
-            'metabolite': 256,
-            'world': 512
-        }
-        _result = 0
-        if feature_list:
-            for feature in feature_list.split(','):
-                _result |= KNOWN_FEATURES[feature]
-        return _result
-
-    def load_chem(self, filename):
-        """
-        Loads the chemical list from filename and returns the appropriate
-        dictionary.  Best to keep under the memory limit.
-        :param filename: file name to read
-        :type filename: str
-        :return: list of chemicals as defined above
-        :rtype: dict
-        """
-        _return = []
-        with open(filename, 'r') as chem_file:
-            chem_file.readline()  # throw out the header row
-            for line in chem_file:
-                chunks = line.split("\t")
-                _return += [
-                    {
-                        'zinc': chunks[1],
-                        'smile': chunks[0],
-                        'tranche': chunks[12],
-                        'charge': chunks[5],
-                        'ph': chunks[6],
-                        'molwt': chunks[7],
-                        'logp': chunks[8],
-                        'reactive': chunks[10],
-                        'features': self.feature_map(chunks[11])
-
-                    }
-                ]
-        return _return
+                    _tranche_files += [f"{tree}/{root_path}/{mid_path}/{file_name}"]
+        with concurrent.futures.ThreadPoolExecutor(max_workers=7) as executor:
+            futures = {executor.submit(load_chem, file_name) for file_name in _tranche_files}
+            for future in concurrent.futures.as_completed(futures):
+                _tranche_data = future.result()
+                self.tranche[_tranche_data[0]] = _tranche_data[1]
+                logging.info("Completed %s", _tranche_data[0])
 
     def path_match(self, filename, expression):
         """
@@ -150,7 +159,8 @@ class ChemLoader:
         """
         return expression.match(filename.split('/')[-1])
 
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    test = ChemLoader("../../files.docking.org/3D/")
+    test = ChemLoader("../../../files.docking.org/3D/")
     print(test.tranche)
