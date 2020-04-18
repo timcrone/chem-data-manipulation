f4df3f6036eb15a2c7321dd53b888a0db50af7f3
Tim Crone <tcrone3@gatech.edu>
2020-04-18 17:19:59 -0400

    Add another small update

--- tools/run-seq.sh
+++ tools/run-seq.sh
@@ -1,2 +1,3 @@
 ./gpusimilarity /mnt/fsim/$1
 cat mols-to-check | awk -v fname=`basename -s .smi.gz.fsim $1` '{print("python3 python/gpusim_search.py "fname" \047\047 \047"$2"\047 >> "$1".sim")}'
+pkill gpusimilarity
