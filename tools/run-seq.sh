100474161ebd856e37ad9dd1121fb63c7e538420
Crone, Timothy David <tcrone3@gatech.edu>
2020-04-19 10:17:34 -0400

    Update run-seq.sh

--- tools/run-seq.sh
+++ tools/run-seq.sh
@@ -1,3 +1,4 @@
-./gpusimilarity /mnt/fsim/$1
+./gpusimilarity /mnt/fsim/$1 &
+sleep 20
 cat mols-to-check | awk -v fname=`basename -s .smi.gz.fsim $1` '{print("python3 python/gpusim_search.py "fname" \047\047 \047"$2"\047 >> "$1".sim")}'
 pkill gpusimilarity
